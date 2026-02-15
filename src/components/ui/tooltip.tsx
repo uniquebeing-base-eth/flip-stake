34afc966216e9309def605684fa08d31b490ee21
Uniquebeing <beingunique049@gmail.com>
2026-02-16 00:23:44 +0100

    Update tooltip.tsx

--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -5,6 +5,7 @@ import { cn } from "@/lib/utils";
 
 const TooltipProvider = TooltipPrimitive.Provider;
 
+
 const Tooltip = TooltipPrimitive.Root;
 
 const TooltipTrigger = TooltipPrimitive.Trigger;
